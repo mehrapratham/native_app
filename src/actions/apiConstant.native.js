431bfb1bf3ad9f0ac8ba796b116ab8af769f6d45
lovetrivedi <loveheenavasu@gmail.com>
2018-07-31 15:39:14 +0530

    create build

--- src/actions/apiConstant.native.js
+++ src/actions/apiConstant.native.js
@@ -1,4 +1,4 @@
-const dev_server = 'http://209.97.142.219:3002';
+const dev_server = 'https://209.97.142.219:3000';
 const BaseUrl = dev_server;
 export const BASE_URL                   =           BaseUrl+':3000/'
 export const GRAPHQL_URL              =           BaseUrl+'/graphiql'
