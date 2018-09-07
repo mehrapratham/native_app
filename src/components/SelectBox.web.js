281b6fd402e7b3583b12e1480f8c3f05e944ff4c
lovetrivedi <loveheenavasu@gmail.com>
2018-09-07 11:10:56 +0530

    reverse years

--- src/components/SelectBox.web.js
+++ src/components/SelectBox.web.js
@@ -7,8 +7,8 @@ export default class SelectBox extends React.Component{
 	render(){
 		return(
 			<select value={this.props.selectedValue} style={{padding: 10, width: '100%', height: 50, fontSize: 20, borderRadius: 0, marginBottom: 20, borderBottom: 'solid 4px #c5c3c4'}} onChange={this.onChange.bind(this)}>
-				<option>{this.props.placeholder}</option>
-				{this.props.list && this.props.list.map((item, key) =>{
+				<option value="">{this.props.placeholder}</option>
+				{this.props.list && this.props.list.reverse().map((item, key) =>{
               	return <option value={item} key={key}>{item}</option>
               })}
 			</select>
